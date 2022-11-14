ae9ed367f0e6f77287624337a962d796e139f54a
Spring Six Jaya <salsabilafitriyah76@gmail.com>
2022-11-14 20:33:02 +0700

    first commit

---
+++ src/utils/Constants.js
@@ -0,0 +1,11 @@
+// settoken untuk reducer user, playlist, playing, player_state, playlist, and id playlist
+export const reducerCases = {
+  SET_TOKEN: "SET_TOKEN",
+  SET_USER: "SET_USER",
+  SET_PLAYLISTS: "SET_PLAYLISTS",
+  SET_PLAYING: "SET_PLAYING",
+  SET_PLAYER_STATE: "SET_PLAYER_STATE",
+  SET_PLAYLIST: "SET_PLAYLIST",
+  SET_PLAYLIST_ID: "SET_PLAYLIST_ID",
+};
+
---
+++ src/utils/StateProvider.jsx
@@ -0,0 +1,12 @@
+// provider for reducer
+import { createContext, useContext, useReducer } from "react";
+
+export const StateContext = createContext();
+
+export const StateProvider = ({ initialState, reducer, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+export const useStateProvider = () => useContext(StateContext);
